741ad874f0ab2d8d36cb79650927b5d7814334fe
oleksandrhavryush <90772851+oleksandrhavryush@users.noreply.github.com>
2021-09-17 10:39:44 +0300

    add home work from lesson 1 with edits

--- HW1/HomeWorkLesson1task1.java
+++ HW1/HomeWorkLesson1task1.java
@@ -2,52 +2,25 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-public class HomeWorkLesson1task3 {
+public class HomeWorkLesson1task1 {
 
-    // Phone calls from three different countries are с1, с2 and с3 standard units per minute.
-    // Talks continued t1, t2 and t3 minutes.
-    // How much computer will count for each call separately and all talk together?
-    // Input all source data from console, make calculations and output to the screen.
+//Flower bed is shaped like a circle.
+// Calculate the perimeter and area by entering the radius.
+// Output obtained results.
 
     public static void main(String[] args) throws IOException {
-
-        String Hungary;
-        String Poland;
-        String Italy;
-
-        int HungaryUnits = 7;
-        int PolandUnits = 12;
-        int ItalyUnits = 23;
-
-        int priceOfAllCalls = 0;
-
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
 
-        for (int i = 0; i < 3; i++) {
-            System.out.println("What is the country?");
+        System.out.println("Напишіть радіус вашої клумби");
 
-            String country = bufferedReader.readLine();
+        double radius = Integer.parseInt(bufferedReader.readLine());
 
-            System.out.println("How many minutes was call?");
+        double perimeter = 2 * 3.14 * radius;
+        double area = 3.14 * Math.pow(radius, 2);
 
-            int minutes = Integer.parseInt(bufferedReader.readLine());
+        System.out.println("Периметер вашої клумби " + perimeter);
+        System.out.println("Площа вашої клумби " + area);
 
-            if (country.contains("Hungary")) {
-                int priceOfCall = HungaryUnits * minutes;
-                priceOfAllCalls = priceOfAllCalls + priceOfCall;
-                System.out.println("Call costs " + priceOfCall);
-            } else if (country.contains("Poland")) {
-                int priceOfCall = HungaryUnits * minutes;
-                priceOfAllCalls = priceOfAllCalls + priceOfCall;
-                System.out.println("Call costs " + priceOfCall);
-            } else if (country.contains("Italy")) {
-                int priceOfCall = HungaryUnits * minutes;
-                priceOfAllCalls = priceOfAllCalls + priceOfCall;
-                System.out.println("Call costs " + priceOfCall);
-            }
-        }
-
-        System.out.println("The price of all calls is " + priceOfAllCalls);
 
     }
 }
