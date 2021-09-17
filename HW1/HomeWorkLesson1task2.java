b58b186dc4785e69950036a0185539f0a482c1eb
oleksandrhavryush <90772851+oleksandrhavryush@users.noreply.github.com>
2021-09-17 10:29:22 +0300

    add home work from lesson 1

---
+++ HW1/HomeWorkLesson1task2.java
@@ -0,0 +1,26 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class HomeWorkLesson1task2 {
+
+    //Define String variables name and address.
+    // Output question "What is your name?"
+    // Read the value name and output next question: “Where are you live, (name)?".
+    // Read address and write whole information.
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+        System.out.println("What is your name?");
+
+        String name = bufferedReader.readLine();
+
+        System.out.println("Where are you live, " + name + "?");
+
+        String address = bufferedReader.readLine();
+
+        System.out.println("Your name is " + name + " and you live in " + address + ".");
+
+    }
+}
---
+++ HW1/HomeWorkLesson1task3.java
@@ -0,0 +1,53 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class HomeWorkLesson1task3 {
+
+    // Phone calls from three different countries are с1, с2 and с3 standard units per minute.
+    // Talks continued t1, t2 and t3 minutes.
+    // How much computer will count for each call separately and all talk together?
+    // Input all source data from console, make calculations and output to the screen.
+
+    public static void main(String[] args) throws IOException {
+
+        String Hungary;
+        String Poland;
+        String Italy;
+
+        int HungaryUnits = 7;
+        int PolandUnits = 12;
+        int ItalyUnits = 23;
+
+        int priceOfAllCalls = 0;
+
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+        for (int i = 0; i < 3; i++) {
+            System.out.println("What is the country?");
+
+            String country = bufferedReader.readLine();
+
+            System.out.println("How many minutes was call?");
+
+            int minutes = Integer.parseInt(bufferedReader.readLine());
+
+            if (country.contains("Hungary")) {
+                int priceOfCall = HungaryUnits * minutes;
+                priceOfAllCalls = priceOfAllCalls + priceOfCall;
+                System.out.println("Call costs " + priceOfCall);
+            } else if (country.contains("Poland")) {
+                int priceOfCall = HungaryUnits * minutes;
+                priceOfAllCalls = priceOfAllCalls + priceOfCall;
+                System.out.println("Call costs " + priceOfCall);
+            } else if (country.contains("Italy")) {
+                int priceOfCall = HungaryUnits * minutes;
+                priceOfAllCalls = priceOfAllCalls + priceOfCall;
+                System.out.println("Call costs " + priceOfCall);
+            }
+        }
+
+        System.out.println("The price of all calls is " + priceOfAllCalls);
+
+    }
+}
