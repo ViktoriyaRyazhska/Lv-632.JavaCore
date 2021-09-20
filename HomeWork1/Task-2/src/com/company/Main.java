ccc0240ff20c02a5ae92e221777686c636970011
Dzhalal <Extezy-boy@ukr.net>
2021-09-20 18:31:34 +0300

    hw1

---
+++ HomeWork1/Task-2/src/com/company/Main.java
@@ -0,0 +1,22 @@
+package com.company;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+
+        public static void main(String[] args) throws IOException {
+            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+            System.out.println("Hello. What is your name?");
+            String name = br.readLine();
+            System.out.println("Where do you live?");
+            String place= br.readLine();
+
+            System.out.println("Hello " + name);
+            System.out.println("You live in " + place);
+            System.out.println("It's grate!");
+        }
+    }
