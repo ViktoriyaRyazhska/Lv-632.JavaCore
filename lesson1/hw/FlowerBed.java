c269a4c7497cc39388d93ca74d01d288aaab34c4
andriymaik <andriymaik@gmail.com>
2021-09-25 14:13:59 +0300

    hw1

--- lesson1/hw/FlowerBed.java
+++ lesson1/hw/FlowerBed.java
@@ -1,16 +1,16 @@
-package introduction;
+package lesson1.hw;
 
 import java.util.Scanner;
 
-public class Circle {
+public class FlowerBed {
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
-		System.out.println("¬вед≥ть рад≥ус");
+		System.out.println("Enter the radius");
 		double radius= scanner.nextDouble();
 		scanner.close();
 		double perimeter = 2 * Math.PI * radius;
 		double area = Math.PI * radius * radius;
-		System.out.print("ѕериметр круга: " + perimeter + ", площадь: " + area);
+		System.out.print("Perimeter = " + perimeter + ", Area = " + area);
 	}
 
 }
