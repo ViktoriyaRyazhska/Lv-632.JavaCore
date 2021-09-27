c30880d6d6b9ab8acc195b5d1131dd0224af554e
Denys-Shl <7860denis@gmail.com>
2021-09-27 22:22:45 +0300

    Init commit CW5

---
+++ src/main/javacore/lesson5/HW5/HW5App.java
@@ -0,0 +1,45 @@
+package lesson5.HW5;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class HW5App {
+    public static void main(String[] args) throws IOException {
+        int[] dayMonth = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        NumberOfMonth(dayMonth, reader);
+        NumberOfMonth_Ver2(dayMonth, reader);
+
+    }
+
+    public static void NumberOfMonth(int[] dayMonth, BufferedReader reader) throws IOException {
+        switch (Integer.parseInt(reader.readLine())) {
+            case 1:
+                System.out.println(dayMonth[0]);
+                break;
+            case 2:
+                System.out.println(dayMonth[1]);
+                break;
+            case 3:
+                System.out.println(dayMonth[2]);
+                break;
+            case 4:
+                System.out.println(dayMonth[3]);
+                break;
+
+            default:
+                System.out.println("Not found month!");
+        }
+    }
+
+    public static void NumberOfMonth_Ver2(int[] dayMonth, BufferedReader reader) throws IOException {
+        int x = Integer.parseInt(reader.readLine());
+        if (x < 13 || x > 0) {
+            System.out.println(dayMonth[Integer.parseInt(reader.readLine()) - 1]);
+        } else {
+            throw new ArrayIndexOutOfBoundsException("Not found month!");
+        }
+    }
+
+}
