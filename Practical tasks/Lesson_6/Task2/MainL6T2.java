cc6eb0348d288e8c5c219958c7d828c6f6cfc02b
Taras Chornyi <garasblack@gmail.com>
2021-10-03 20:03:22 +0300

    HW5 and 6 is done

--- Practical tasks/Lesson_6/Task2/MainL6T2.java
+++ Practical tasks/Lesson_6/Task2/MainL6T2.java
@@ -5,9 +5,9 @@ public class MainL6T2 {
         Line [] lines = new Line[2];
 
 
-        lines[0] = new Line(new Point(14,15), new Point(47,15));
+        lines[0] = new Line(new Point(14,15), new Point(46,15));
 
-        lines[1] = new ColorLine(new Point(27,46), new Point(41,57), "Green");
+        lines[1] = new ColorLine(new Point(27,43), new Point(41,57), "Green");
 
         for (int i=0; i<lines.length; i++){
             System.out.println(lines[i]);
