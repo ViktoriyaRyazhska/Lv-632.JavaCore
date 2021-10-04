3d90d81fe8a413ea6377fab354074e1f9576c951
Denys.Sh <7860denis@gmail.com>
2021-10-04 10:22:02 +0300

    addde test

---
+++ src/test/lesson5/HW5/HW5AppTest.java
@@ -0,0 +1,16 @@
+package lesson5.HW5;
+
+import org.junit.jupiter.api.Test;
+
+class HW5AppTest {
+
+    @Test
+    void enteringNotNegativNumbers() {
+        int[] arr = {2, 2, 3, 2, -5};
+        for (int i = 0; i < arr.length; i++) {
+//            HW5App.enteringNotNegativNumbers(arr[i]);
+        }
+        String actual = "Result:  8";
+
+    }
+}
