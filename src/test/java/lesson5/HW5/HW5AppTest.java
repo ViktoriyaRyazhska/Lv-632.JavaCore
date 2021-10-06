65a61f53a5d0839c5263bc11562cd2afb101cbaa
Denys.Sh <7860denis@gmail.com>
2021-10-06 13:54:28 +0300

    HW5 Test with Mokito. Mock BufferedReader

--- src/test/java/lesson5/HW5/HW5AppTest.java
+++ src/test/java/lesson5/HW5/HW5AppTest.java
@@ -2,38 +2,25 @@ package lesson5.HW5;
 
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.junit.runner.RunWith;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
-//@RunWith(JUnitPlatform.class)
 class HW5AppTest {
     HW5 hw5 = new HW5();
     int[] dayMonth = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
-    BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
 
-    @Mock
-    private BufferedReader bufferedReader; // = new BufferedReader(new InputStreamReader(System.in));
+    BufferedReader bufferedReader = Mockito.mock(BufferedReader.class);
 
     @Test
     void numberOfMonth() throws IOException {
-        verify(bufferedReader.readLine().isEmpty());
-        assertNotNull(bufferedReader.readLine());
         when(bufferedReader.readLine()).thenReturn("1");
-
-        System.out.println(bufferedReader.readLine());
-
         int expected = hw5.NumberOfMonth(dayMonth, bufferedReader);
         int actual = 31;
         assertEquals(expected, actual);
@@ -41,7 +28,8 @@ class HW5AppTest {
 
     @Test
     void numberOfMonth_Ver2() throws IOException {
-        int expected = hw5.NumberOfMonth_Ver2(dayMonth, reader);
+        when(bufferedReader.readLine()).thenReturn("1");
+        int expected = hw5.NumberOfMonth_Ver2(dayMonth, bufferedReader);
         int actual = 31;
         assertEquals(expected, actual);
     }
@@ -64,7 +52,8 @@ class HW5AppTest {
 
     @Test
     void enteringNotNegativNumbers() throws IOException {
-        int expected = hw5.enteringNotNegativNumbers(reader);
+        when(bufferedReader.readLine()).thenReturn("2","2","2","-5");
+        int expected = hw5.enteringNotNegativNumbers(bufferedReader);
         int actual = 8;
         assertEquals(expected, actual);
     }
