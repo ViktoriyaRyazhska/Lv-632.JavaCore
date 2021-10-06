c86cdd9da457c8393b78637e090ba4eafbae7b33
Denys.Sh <7860denis@gmail.com>
2021-10-06 18:59:01 +0300

    added CW8 and fix HW7

--- src/main/javacore/lesson7/HW7/HW7App.java
+++ src/main/javacore/lesson7/HW7/HW7App.java
@@ -6,7 +6,6 @@ import lesson7.HW7.model.Payment;
 import lesson7.HW7.model.SalariedEmployee;
 
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
 
@@ -17,13 +16,6 @@ public class HW7App {
         employeeList.add(new SalariedEmployee(103, "Alex", "Ad", 110.5, 30.0, "EC4577"));
         employeeList.add(new ContractEmployee(102, "Jony", "Reg", 12589, 1100));
         employeeList.add(new ContractEmployee(104, "Jons", "Groov", 12541, 1050.45));
-
-//        Collections.sort(employeeList, new Comparator<Employee>() {
-//            @Override
-//            public int compare(Employee o1, Employee o2) {
-//                return o1.getId() - o2.getId();
-//            }
-//        });
 
         employeeList.sort(Comparator.comparing(Payment::calculatePay));
         employeeList.forEach(employee -> System.out.println(employee));
