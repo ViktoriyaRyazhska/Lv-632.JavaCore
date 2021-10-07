44d12c252055ab00b5079b47e0c90c7b96d354e0
Denys.Sh <7860denis@gmail.com>
2021-10-07 16:54:34 +0300

    added HW8 task1 and task2

---
+++ src/main/javacore/lesson8/HW8/task1/FullName.java
@@ -0,0 +1,30 @@
+package lesson8.HW8.task1;
+
+public class FullName {
+   private String firstName;
+   private String lastName;
+
+    public FullName() {
+    }
+
+    public FullName(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+}
