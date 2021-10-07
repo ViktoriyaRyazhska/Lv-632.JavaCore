59fe84fb5651efac6a2f60853932c6eddf8eeee7
vullok <andriy.dep@gmail.com>
2021-10-07 13:43:17 +0300

    Task 7

--- HW5/Task06/Task06.java
+++ HW5/Task06/Task06.java
@@ -10,6 +10,8 @@ public class Task06 {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int productP = 1;
 		int inputN = 0;
+		int countN = 0;
+		int countE = 0;
 
 		System.out.println("Please enter a positive number. \nOr enter a negative number to exit!:");
 		boolean flag;
@@ -19,9 +21,11 @@ public class Task06 {
 				if (inputN < 0) {
 					break;
 				} else {
-					if ((inputN % 2) == 0) {
+					if ((inputN ^ 1) == inputN + 1) {
 						productP = productP * inputN;
+						countE++;
 					}
+					countN++;
 				}
 				flag = true;
 			} catch (Exception e) {
@@ -29,6 +33,7 @@ public class Task06 {
 				flag = false;
 			}
 		}
+		System.out.println("Numbers entered, TOTAL: " + countN + ". From them -  " + countE + " even numbers");
 		System.out.println("Count the product of all entered even numbers: " + productP);
 	}
 }
