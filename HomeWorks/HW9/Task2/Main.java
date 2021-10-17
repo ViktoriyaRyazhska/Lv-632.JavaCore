d648e3499d978c0bd20648551175cd72e096ea92
Taras Chornyi <garasblack@gmail.com>
2021-10-17 15:26:14 +0300

    HW9. Adjusted Task 2

--- HomeWorks/HW9/Task2/Main.java
+++ HomeWorks/HW9/Task2/Main.java
@@ -10,35 +10,34 @@ public class Main {
         personMap.put( "Baggins", "Bilbo");
         personMap.put( "Zub", "Orest");
         personMap.put( "Grey", "Gandalf");
-        personMap.put( "Oakenshield", "Orest");
+        personMap.put( "Oakenshield", "Torin");
         personMap.put("Isildorovich", "Aragorn");
         personMap.put("Brown", "Radagast");
         personMap.put("Tranduilovich","Legolas");
         personMap.put("Filarfinovna", "Galadriel");
-        personMap.put( "Erendilovich", "Elrond");
+        personMap.put( "Tuk", "Bilbo");
 
         System.out.println(personMap.entrySet());
         Main.sameName(personMap);
         Main.removePerson(personMap, "Orest");
 
     }
-        private static void sameName (Map<String, String> map){     //Method that found same values in Map, adding it in the new list and counting number of thees names;
-            List<String> names = new ArrayList<>(map.values());
-            List<String> doubleNames = new ArrayList<>();
-            int count = 1;
-            doubleNames.add("Empty");
-            for (int i=1; i<names.size();i++) {
-                if (names.get(i).equals(names.get(i - 1))) {
-                    doubleNames.set(0, names.get(i));
-                    doubleNames.add(names.get(i - 1));
-                    count++;
-                }
+
+        private static void sameName (Map < String, String > map) {                 //Method that found duplicate values in Map
+        Set <String> filter = new HashSet<>();                                      //Step 1 - finding duplicates
+        Set <String> duplicates = new HashSet<>();
+        for(Map.Entry<String, String> person:map.entrySet()) {
+            if (!filter.add(person.getValue()))
+                duplicates.add(person.getValue());
+        }
+            for(String name:duplicates){                                            //Step 2- printing all duplicates from Map
+                for (Map.Entry <String, String> person: map.entrySet()){
+                if (person.getValue().equals(name))
+                    System.out.println(person);
             }
-                if(count>1){
-                    System.out.println(count+" same names is found. The list oft thees names:");
-                    for (String name: doubleNames) System.out.print(name+", ");
         }
     }
+
 
         private static void removePerson (Map<String, String> map, String name) {       //Method removing all elements with some value
             Map<String, String> map2 = new HashMap<>(map);
