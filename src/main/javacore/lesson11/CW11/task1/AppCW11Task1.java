ca4321683bb0591ba2ad856d110ddc7da08ae955
Denys.Sh <7860denis@gmail.com>
2021-10-18 13:17:33 +0300

    added CW11 task1

---
+++ src/main/javacore/lesson11/CW11/task1/AppCW11Task1.java
@@ -0,0 +1,36 @@
+package lesson11.CW11.task1;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class AppCW11Task1 {
+    public static void main(String[] args) {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        int result = squareRectangle(shouldReturnNumber(bufferedReader), shouldReturnNumber(bufferedReader));
+        System.out.println(result);
+    }
+
+    private static int squareRectangle(int a, int b) {
+        return a * b;
+    }
+
+    private static int shouldReturnNumber(BufferedReader reader) {
+        System.out.println("Enter number: ");
+        int result = 0;
+        int num = 0;
+        try {
+            num = Integer.parseInt(reader.readLine());
+            if (num > 0) {
+                result = num;
+            } else if (num == 0){
+
+            } else {
+                throw new IllegalArgumentException("number < 0!");
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return result;
+    }
+}
--- src/main/javacore/lesson9/HW9/task3/AppHW9Task3.java
+++ src/main/javacore/lesson9/HW9/task3/AppHW9Task3.java
@@ -7,7 +7,6 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.Comparator;
-import java.util.Iterator;
 import java.util.List;
 
 public class AppHW9Task3 {
