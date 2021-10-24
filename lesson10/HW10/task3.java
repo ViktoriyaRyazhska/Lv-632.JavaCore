0cc0a4bef274db5e5d90cf7f65d8e4ef2515a4a9
unknown <kaa230493@gmail.com>
2021-10-24 13:19:00 +0300

    rework  3 task

--- lesson10/HW10/task3.java
+++ lesson10/HW10/task3.java
@@ -7,16 +7,19 @@ import java.util.regex.Pattern;
 public class task3 {
 
 	public static void main(String[] args) {
-
+		int count = 0;
 		Scanner scan = new Scanner(System.in);
 		System.out.println("Entre sentences...");
 		String text = scan.nextLine();
 		Pattern pat = Pattern.compile("\\$(\\d*)(\\.\\d{2})");
 		Matcher match = pat.matcher(text);
+		String[] mass = text.split(" ");
 
-		if (match.find()) {
-			System.out.println(match.group());
+		for (int i = 0; i < mass.length; i++) {
+			if (match.find()) {
+				count++;
+				System.out.println(match.group()+ ", number of coincidences: "+count);
+			}
 		}
 	}
-
 }
