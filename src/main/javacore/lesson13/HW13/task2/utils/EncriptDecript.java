d70527f30cbac30d101b2b8670f0b979f370420b
Denys.Sh <7860denis@gmail.com>
2021-10-29 15:59:15 +0300

    fix HW13 task2

--- src/main/javacore/lesson13/HW13/task2/utils/EncriptDecript.java
+++ src/main/javacore/lesson13/HW13/task2/utils/EncriptDecript.java
@@ -10,8 +10,7 @@ public class EncriptDecript {
             "1234567890+_)(*&^%$#@!`|?}{-.,;";
 
     public String encrypt(String text, int key) {
-        LocalDateTime dateTime = LocalDateTime.now();
-        System.out.println("Encryption start ! " + dateTime.format(DateTimeFormatter.ofPattern("dd:MM:yyyy - HH:mm:ss")));
+        System.out.println("Encryption start ! " + LocalDateTime.now().format(DateTimeFormatter.ofPattern("dd:MM:yyyy - HH:mm:ss")));
         int dictLength = DICT.length();
         int textLength = text.length();
         String result = "";
@@ -27,13 +26,12 @@ public class EncriptDecript {
             }
         }
         System.out.println(result);
-        System.out.println("Encryption complete! " + dateTime.format(DateTimeFormatter.ofPattern("dd:MM:yyyy - HH:mm:ss")));
+        System.out.println("Encryption complete! " + LocalDateTime.now().format(DateTimeFormatter.ofPattern("dd:MM:yyyy - HH:mm:ss")));
         return result;
     }
 
     public String decrypt(String text, int key) {
-        LocalDateTime dateTime = LocalDateTime.now();
-        System.out.println("Decription start ! " + dateTime.format(DateTimeFormatter.ofPattern("dd:MM:yyyy - HH:mm:ss")));
+        System.out.println("Decription start ! " + LocalDateTime.now().format(DateTimeFormatter.ofPattern("dd:MM:yyyy - HH:mm:ss")));
         int dictLength = DICT.length();
         int textLength = text.length();
         String result = "";
@@ -49,7 +47,7 @@ public class EncriptDecript {
             }
         }
         System.out.println(result);
-        System.out.println("Decription complete! " + dateTime.format(DateTimeFormatter.ofPattern("dd:MM:yyyy - HH:mm:ss")));
+        System.out.println("Decription complete! " + LocalDateTime.now().format(DateTimeFormatter.ofPattern("dd:MM:yyyy - HH:mm:ss")));
         return result;
     }
 }
