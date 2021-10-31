461f32858f6cd6850a581b754cdeac2483d9c5e4
Denys.Sh <7860denis@gmail.com>
2021-10-31 15:58:32 +0200

    refactoring HW13 task3

--- src/main/javacore/lesson13/HW13/task3/AppHW13Task3.java
+++ src/main/javacore/lesson13/HW13/task3/AppHW13Task3.java
@@ -6,12 +6,13 @@ import java.util.regex.Pattern;
 public class AppHW13Task3 {
     public static void main(String[] args) {
         String string = "30-10-2021";
-        if (getMathesBirthDay(string)){
+        if (getMathesBirthDay(string)) {
             System.out.println("Date true !!! " + string);
         }
     }
+
     public static boolean getMathesBirthDay(String s) {
-Pattern p = Pattern.compile("^[0-3]?[0-9].[0-3]?[0-9].(?:[0-9]{2})?[0-9]{2}$");
+        Pattern p = Pattern.compile("^[0-3]?[0-9].[0-3]?[0-9].(?:[0-9]{2})?[0-9]{2}$");
         Matcher m = p.matcher(s);
         return m.matches();
     }
