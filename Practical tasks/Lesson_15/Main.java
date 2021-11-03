0ef05885df35817b8ce3db5ff3b5b97a705077b6
Taras Chornyi <garasblack@gmail.com>
2021-11-03 17:23:38 +0200

    comment

---
+++ Practical tasks/Lesson_15/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+
+    /*
+    4.	Input string and calculate number of loud letters (e.g. word: “My test text”, result: 3)
+     */
+    public void four (String text){
+
+    }
+
+}
