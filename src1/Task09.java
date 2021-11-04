7351f54896d8d64423c78aa60b0e1db2d9c2aff1
vullok <andriy.dep@gmail.com>
2021-11-04 23:33:18 +0200

    Task 9

--- src1/Task09.java
+++ src1/Task09.java
@@ -7,8 +7,9 @@ public class Task09 {
 
 	  public static void main(String[] args) {
 
-			int rand_int;
+			int rand_int = 0;
 			int n = 0;
+			int countR = 0;
 			Random rand = new Random();
 			BufferedReader br = new BufferedReader(new InputStreamReader((System.in)));
 			System.out.print("Enter RANDOM number: ");
@@ -21,9 +22,14 @@ public class Task09 {
 
 			for (int i = 1; i <= n; i++) {
 				  rand_int = rand.nextInt(2);
+					if (rand_int == 1) {
+						  countR++;
+					}
 				  System.out.println("0 or 1 Random Number: " + rand_int);
 			}
-//			if (rand_int = 1)
+
+			  System.out.println("-------------------------------");
+			  System.out.println("Number ONE generates: " + countR + " times");
 	  }
 
 }
