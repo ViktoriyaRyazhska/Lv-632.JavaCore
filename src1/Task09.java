adaba7220612356c902c43dd4cbb1f16f6c2c5af
vullok <andriy.dep@gmail.com>
2021-11-04 23:07:22 +0200

    Task 09

--- src1/Task09.java
+++ src1/Task09.java
@@ -1,14 +1,29 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Random;
 
 public class Task09 {
 
 	  public static void main(String[] args) {
-		System.out.println();
-		
-		
-		
-		System.out.println();
-		
 
+			int rand_int;
+			int n = 0;
+			Random rand = new Random();
+			BufferedReader br = new BufferedReader(new InputStreamReader((System.in)));
+			System.out.print("Enter RANDOM number: ");
+
+			try {
+				  n = Integer.parseInt(br.readLine());
+			} catch (IOException e) {
+				  e.printStackTrace();
+			}
+
+			for (int i = 1; i <= n; i++) {
+				  rand_int = rand.nextInt(2);
+				  System.out.println("0 or 1 Random Number: " + rand_int);
+			}
+//			if (rand_int = 1)
 	  }
 
 }
