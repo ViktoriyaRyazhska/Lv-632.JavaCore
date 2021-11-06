9c54fd2863510e909bdf3a1903755da7b03c0a27
Андрей <kaa230493@gmail.com>
2021-11-06 10:57:52 +0200

    rework task7, only print result

--- src1/Task07.java
+++ src1/Task07.java
@@ -7,23 +7,26 @@ import java.util.List;
 public class Task07 {
 
 	static BufferedReader br = new BufferedReader(new InputStreamReader((System.in)));
-	
+
 	public static void execute7() {
 		System.out.println("Input a number to define the factorial: ");
-		int number=0;
+		int number = 0;
 		try {
-			number=Integer.parseInt(br.readLine());
-		} catch (NumberFormatException | IOException  e) {
+			number = Integer.parseInt(br.readLine());
+		} catch (NumberFormatException | IOException e) {
 			e.printStackTrace();
-		} 
-		List<Integer> parts=new ArrayList<>();
-		long temp=1;
-		for(int i=1;i<=number;i++) {
-			temp*=i;
+		}
+		List<Integer> parts = new ArrayList<>();
+		long temp = 1;
+		for (int i = 1; i <= number; i++) {
+			temp *= i;
 			parts.add(i);
 		}
-		System.out.println(parts);
-		System.out.println(temp);
+		System.out.print(number + "!= ");
+		for (int a = 0; a < parts.size(); a++) {
+			System.out.print(parts.get(a) + "*");
+		}
+		System.out.print("=" + " " + temp);
 	}
 
 }
